15aa2aa742467315d469a3ac6486f00949848c90
cr-trojan23 <pandusrinu1604@gmail.com>
2021-10-20 16:05:09 +0530

    [polybar] removed useless files

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/forest"
+
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
