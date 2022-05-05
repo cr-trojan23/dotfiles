31150485290dffacfc533aca5bab7f307ec953f8
cr-trojan23 <pandusrinu1604@gmail.com>
2022-05-05 22:56:08 +0530

    Update i3 config, add brave autostart

--- .config/i3/braveas.sh
+++ .config/i3/braveas.sh
@@ -1,5 +1,7 @@
-brave-browser http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://captive.apple.com/hotspot-detect.html &
-sleep 6
+brave-browser http://phc.prontonetworks.com/cgi-bin/authlogin? &
+sleep 5
 pkill brave
 pkill brave-browser
-brave-browser https://web.whatsapp.com/ https://mail.google.com/mail/u/0/?ogbl#inbox &
+brave-browser &
+reload
+restart
